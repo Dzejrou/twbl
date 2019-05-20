00ad655540a0f4a2815c8fecb34dc8fb55d4aebe
Jaroslav Jindrak <dzejrou@gmail.com>
2019-05-20 15:07:08 +0200

    add twbl implementation

---
+++ twbl.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " {,+,-}N\n";
+        return 1;
+    }
+
+    std::istringstream iss{argv[1]};
+    int val{};
+
+    if (!(iss >> val))
+    {
+        std::cerr << "Argument " << argv[1] << " is not a valid number.\n";
+        return 1;
+    }
+
+    std::string fname{"/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-eDP-1/intel_backlight/brightness"};
+
+    std::ifstream ifs{fname};
+
+    int curr{};
+    if (!(ifs >> curr))
+    {
+        std::cerr << "Could not read current brightness value.\n";
+        return 1;
+    }
+    ifs.close();
+
+    val += curr;
+    std::ofstream ofs{fname};
+    if (!(ofs << std::to_string(val)))
+    {
+        std::cerr << "Could not write new brightness value.\n";
+        return 1;
+    }
+
+    return 0;
+}
